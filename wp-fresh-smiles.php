88ef6963bccd12952e32256698cbb23681dcc856
Kai Armstrong <kai.armstrong@gmail.com>
2014-04-02 11:48:08 -0500

    main plugin file

---
+++ wp-fresh-smiles.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: WP Fresh Smiles
+Plugin URI: https://github.com/pressable/wp-fresh-smiles
+Description: A WP Plugin of the Fresh Smiles application for displaying FreshDesk customer satisfaction.
+Author: A. Kai Armstrong
+Version: 0.1.0
+Author URI: http://www.kaiarmstrong.com
+*/
+
+
